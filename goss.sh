08b2ee711ac2258b17b18c98edfd975a97e6a490
Stuart Raynor <madstu@gmail.com>
2018-06-08 10:22:38 +0100

    Add files via upload

--- goss.sh
+++ goss.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # MadStu's Small Install Script
 cd ~
-wget https://raw.githubusercontent.com/MadStu/Dextro/master/newgossmn.sh
+wget https://raw.githubusercontent.com/MadStu/GOSS/master/newgossmn.sh
 chmod 777 newgossmn.sh
 sed -i -e 's/\r$//' newgossmn.sh
 ./newgossmn.sh
