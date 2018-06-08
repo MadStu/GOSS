7b1393e1f014c753a5685cb770acb6c18835a4a4
Stuart Raynor <madstu@gmail.com>
2018-06-08 10:24:14 +0100

    Delete goss.sh

--- goss.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# MadStu's Small Install Script
-cd ~
-wget https://raw.githubusercontent.com/MadStu/GOSS/master/newgossmn.sh
-chmod 777 newgossmn.sh
-sed -i -e 's/\r$//' newgossmn.sh
-./newgossmn.sh
